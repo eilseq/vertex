6c9b084e2dcbb2336b3a6b413d659395762eef00
eilseq <tools@eilseq.com>
2023-05-30 15:53:48 +0200

    update linter and config files

--- next.config.js
+++ next.config.js
@@ -4,6 +4,30 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value:
+              "worker-src 'self' blob:; script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
