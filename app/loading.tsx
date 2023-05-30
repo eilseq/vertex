aa010022efbd7e06d7353812c700650815ca0b6e
eilseq <tools@eilseq.com>
2023-05-30 16:39:28 +0200

    main page

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import Token from '#/ui/token';
+
+const DEFAULT_HASH = 'ooDefault';
+
+export default function Loading() {
+  return (
+    <>
+      <Token hash={DEFAULT_HASH} />
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,83 @@
-import { demos } from '#/lib/demos';
-import Link from 'next/link';
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+import Token from '#/ui/token';
+
+const DEFAULT_HASH = 'ooDefault';
 
 export default function Page() {
+  const router = useRouter();
+
+  const [ready, setReady] = useState(false);
+  const [hover, setHover] = useState(false);
+  const [leaving, setLeaving] = useState(false);
+
+  const letTitleSlideIn = () =>
+    setTimeout(() => {
+      setReady(true);
+    }, 800);
+
+  const letTitleBlurOut = () => {
+    setLeaving(true);
+    setReady(false);
+    router.push('/collection');
+  };
+
+  useEffect(() => {
+    letTitleSlideIn();
+  }, []);
+
   return (
-    <div className="space-y-8">
-      <h1 className="text-xl font-medium text-gray-300">Examples</h1>
-
-      <div className="space-y-10 text-white">
-        {demos.map((section) => {
-          return (
-            <div key={section.name} className="space-y-5">
-              <div className="text-xs font-semibold uppercase tracking-wider text-gray-400">
-                {section.name}
-              </div>
-
-              <div className="grid grid-cols-1 gap-5 lg:grid-cols-2">
-                {section.items.map((item) => {
-                  return (
-                    <Link
-                      href={`/${item.slug}`}
-                      key={item.name}
-                      className="group block space-y-1.5 rounded-lg bg-gray-900 px-5 py-3 hover:bg-gray-800"
-                    >
-                      <div className="font-medium text-gray-200 group-hover:text-gray-50">
-                        {item.name}
-                      </div>
-
-                      {item.description ? (
-                        <div className="text-sm text-gray-400 line-clamp-3 group-hover:text-gray-300">
-                          {item.description}
-                        </div>
-                      ) : null}
-                    </Link>
-                  );
-                })}
-              </div>
-            </div>
-          );
-        })}
+    <>
+      <div
+        className="space-y-4 text-white mix-blend-difference"
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+      >
+        <div className="cursor-pointer" onClick={(e) => letTitleBlurOut()}>
+          <div className="text-center text-[100pt] font-bold">
+            {!ready && !leaving && <TitleIn />}
+            {ready && !leaving && hover && <TitlePulse />}
+            {ready && !leaving && !hover && <TitleFlickIn />}
+            {leaving && <TitleOut />}
+          </div>
+        </div>
       </div>
-    </div>
+      <Token hash={DEFAULT_HASH} />
+    </>
   );
 }
+
+const TitleIn = () => (
+  <>
+    <h1 className="animate-fadeToOpacityFromLeft delay-1000">VERTEX</h1>
+    <h2 className="animate-fadeToOpacityFromRight text-xl delay-700">
+      Generative Art Collection
+    </h2>
+  </>
+);
+
+const TitlePulse = () => (
+  <>
+    <h1 className="animate-sonar text-green-500">VERTEX</h1>
+    <h2 className="animate-sonarFast text-xl delay-[200ms]">Start Exploring</h2>
+  </>
+);
+
+const TitleFlickIn = () => (
+  <>
+    <h1 className="animate-fastFlicker">VERTEX</h1>
+    <h2 className="animate-superFastFlicker text-xl delay-[200ms]">
+      Generative Art Collection
+    </h2>
+  </>
+);
+
+const TitleOut = () => (
+  <>
+    <h1 className="animate-blurOut">VERTEX</h1>
+    <h2 className="animate-blurOut text-xl">Generative Art Collection</h2>
+  </>
+);
