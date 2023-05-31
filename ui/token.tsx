32de72d736ddd5f0e514fd00ec6928d332ecf95c
eilseq <tools@eilseq.com>
2023-05-31 12:06:13 +0200

    optimize iframe token render

--- ui/token.tsx
+++ ui/token.tsx
@@ -1,14 +1,26 @@
 'use client';
 
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
-const Token = ({ seed }: { seed?: string }) => {
+const Token = React.memo(({ seed }: { seed?: string }) => {
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+
+  useEffect(() => {
+    if (iframeRef.current) {
+      iframeRef.current.src = seed
+        ? `/v0/index.html?seed=${seed}`
+        : `/v0/index.html`;
+    }
+  }, [seed]);
+
   return (
     <iframe
+      ref={iframeRef}
       className="fixed left-0 right-0 top-0 -z-10 h-screen w-screen"
-      src={seed ? `/v0/index.html?seed=${seed}` : `/v0/index.html`}
     />
   );
-};
+});
+
+Token.displayName = 'Token'; // Set the display name
 
 export default Token;
